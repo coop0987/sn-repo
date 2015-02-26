cf5176d3e008e01b9973eef91b7e4c54f85057b7
joseuribe <jse.uribe@gmail.com>
2015-02-25 23:41:02 -0500

    change to mailgun

--- config/environment.rb
+++ config/environment.rb
@@ -2,14 +2,14 @@
 require File.expand_path('../application', __FILE__)
 
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'sweatnet.heroku.com',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'sweatnet.com',
-  :enable_starttls_auto => true
 }
+ActionMailer::Base.delivery_method = :smtp
 
 # Initialize the Rails application.
 Sweatnet::Application.initialize!
