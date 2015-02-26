0cd20408b546079baa5cd09cca3022fc28f4adad
joseuribe <jse.uribe@gmail.com>
2015-02-25 22:44:27 -0500

    checking precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Sweatnet::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.paths << Rails.root.join( 'assets')
 end
