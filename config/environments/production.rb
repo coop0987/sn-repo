946bd7fce1e2286fd2443c8b0befcd4ec9c23fd1
joseuribe <jse.uribe@gmail.com>
2015-02-27 11:31:45 -0500

    fonts to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Sweatnet::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.assets.paths << Rails.root.join( 'assets')
+  config.assets.paths << Rails.root.join( 'assets', "assets/fonts")
 end
