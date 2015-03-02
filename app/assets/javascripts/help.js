7d9027e16f2924f512aa1ca3f6b3fe47ef92923e
joseuribe <jse.uribe@gmail.com>
2015-03-02 10:38:25 -0500

    add lato to fonst

---
+++ app/assets/javascripts/help.js
@@ -0,0 +1,7 @@
+function centerModal() {
+    $(this).css('display', 'block');
+    var $dialog = $(this).find(".modal-dialog");
+    var offset = ($(window).height() - $dialog.height()) / 2;
+    // Center modal vertically in window
+    $dialog.css("margin-top", offset);
+}
