4f0ff661a55f44daddb2a790f7f4be9e14422474
Vito Ao <victorsummer@gmail.com>
2015-03-07 14:00:05 +0000

    add polymer

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,18 @@ Sweatnet::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  namespace :api, defaults: { format: "json" } do
+    devise_for :users, :controllers => { :sessions=> "api/sessions"}
+    resources :subscriptions, :only => [:create] do
+    end
+  end
+
   # You can have the root of your site routed with "root"
-   root 'under_construction#index'
-   namespace :api, defaults: { format: "json" } do
-     devise_for :users, :controllers => { :sessions=> "api/sessions"}
-     resources :subscriptions, :only => [:create] do
-     end
-   end
-   
-   resources :home, :only => [:index]
-   
+  root 'home#index'
+  resources :home, :only => [:index]
+
   #get '/alpha-login' => 'under_construction#alpha', as: :alpha_login
-   
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
